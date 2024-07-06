0075faf6737406ac208a54259ff714863fb717c6
koji_kawamura <45913424+kouji0705@users.noreply.github.com>
2024-07-06 10:02:11 +0900

    テキストボックスでの検索実施 (#1)
    * fix:
    
    * fix:

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,5 @@
 import 'package:flutter/material.dart';
-
 import 'api_service.dart';
-
-void main() {
-  runApp(PokemonApp());
-}
-
-class PokemonApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Pokemon App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: PokemonHomePage(),
-    );
-  }
-}
 
 class PokemonHomePage extends StatefulWidget {
   @override
@@ -29,6 +11,7 @@ class _PokemonHomePageState extends State<PokemonHomePage> {
   Map<String, dynamic>? _pokemonData;
   bool _isLoading = false;
   String? _error;
+  final TextEditingController _controller = TextEditingController();
 
   @override
   void initState() {
@@ -57,49 +40,83 @@ class _PokemonHomePageState extends State<PokemonHomePage> {
     }
   }
 
+  void _fetchPokemon() async {
+    setState(() {
+      _isLoading = true;
+      _error = null;
+    });
+    try {
+      final data =
+          await _apiService.fetchPokemon(_controller.text.toLowerCase());
+      setState(() {
+        _pokemonData = data;
+      });
+    } catch (e) {
+      setState(() {
+        _error = e.toString();
+      });
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pokemon App'),
-      ),
-      body: Center(
-        child: _isLoading
-            ? CircularProgressIndicator()
-            : _error != null
-                ? Text('Error: $_error')
-                : _pokemonData != null
-                    ? Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            _pokemonData!['name'].toString().toUpperCase(),
-                            style: TextStyle(
-                              fontSize: 36,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 20),
-                          if (_pokemonData!['sprites'] != null &&
-                              _pokemonData!['sprites']['front_default'] != null)
-                            Image.network(
-                              _pokemonData!['sprites']['front_default'],
-                              height: 200,
-                              width: 200,
-                            ),
-                          SizedBox(height: 20),
-                          Text(
-                            'Height: ${_pokemonData!['height']}',
-                            style: TextStyle(fontSize: 24),
-                          ),
-                          Text(
-                            'Weight: ${_pokemonData!['weight']}',
-                            style: TextStyle(fontSize: 24),
-                          ),
-                        ],
-                      )
-                    : Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          // スクロール可能なコンテナでラップする
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start, // 変更点: 上部から開始
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  labelText: 'Enter Pokemon Name or Number',
+                  border: OutlineInputBorder(), // 境界線を追加
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _fetchPokemon,
+                child: Text('Fetch Pokemon'),
+              ),
+              SizedBox(height: 20),
+              if (_isLoading) CircularProgressIndicator(),
+              if (_error != null) Text('Error: $_error'),
+              if (_pokemonData != null) ...[
+                Text(
+                  _pokemonData!['name'].toString().toUpperCase(),
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 20),
+                if (_pokemonData!['sprites'] != null &&
+                    _pokemonData!['sprites']['front_default'] != null)
+                  Image.network(
+                    _pokemonData!['sprites']['front_default'],
+                    height: 200,
+                    width: 200,
+                  ),
+                SizedBox(height: 20),
+                Text(
+                  'Height: ${_pokemonData!['height']}',
+                  style: TextStyle(fontSize: 24),
+                ),
+                Text(
+                  'Weight: ${_pokemonData!['weight']}',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ],
+            ],
+          ),
+        ),
       ),
     );
   }
